489c654f6cab525829d438c1efae5598ba9e8889
BharadwajShivanya <144115901+BharadwajShivanya@users.noreply.github.com>
2025-07-09 20:21:49 +0530

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,17 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById('root')
+);
